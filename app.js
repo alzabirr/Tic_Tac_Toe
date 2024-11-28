db73e987c418bb3108bc4c478595ac2eded8e29c
alzabirr <alzabirhussain@gmail.com>
2024-11-29 05:34:34 +0600

    done

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let boxes = document.querySelectorAll(".box");
 let reset_btn = document.querySelector("#reset_btn");
+let newGameBtn = document.querySelector("#new-btn")
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-let turn_0 = true;
+
+
+
+let turn0 = true;
 
 const winPatterns = [
     [0,1,2],
@@ -14,10 +20,73 @@ const winPatterns = [
     [2,4,6],
 ];
 
+const resetGame = ()=>{
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide")
+
+};
+
 // click box 
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
         console.log("box was clicked");
-        box.innerText = "XN"
+
+    
+        if(turn0){
+            box.innerText = "😎";
+            turn0 = false;
+        }else{
+            box.innerText = "🙂";
+            turn0 = true;
+
+        }
+        box.disabled = true;
+        checkwinner();
     });
 });
+
+const disabledBoxes = ()=> {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = ()=> {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = ""
+    }
+}
+
+
+
+ const showWinner = (winner) => {
+   
+    msg.innerText = `Congratutations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+
+ };
+ 
+
+const checkwinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText; 
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val=== pos3Val ){
+                console.log("winner",pos1Val);
+                
+                showWinner(pos1Val);
+            }
+        }  
+
+    }
+    
+};
+
+newGameBtn.addEventListener("click", resetGame);
+reset_btn.addEventListener("click",resetGame);
